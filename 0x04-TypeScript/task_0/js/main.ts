ddbdb85bd3fbf9f3d0e56440f20dfb401fe7791d
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-12-12 12:46:19 +0400

    task_0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,38 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 18,
+    location: 'New York'
+};
+
+const student2: Student = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 19,
+    location: 'California'
+};
+
+const studentsList: Array<Student> = [student1, student2];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+studentsList.forEach((student) => {
+    const row = document.createElement('tr');
+    const nameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+});
+
+table.appendChild(tbody);
+document.body.appendChild(table);
